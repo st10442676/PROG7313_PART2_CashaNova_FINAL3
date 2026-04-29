7466f38216894dd005e99fa08999a2090cbd5826
ST10449016 <167231882+ST10449016@users.noreply.github.com>
2026-04-29 17:52:59 +0200

    COMMIT 7- NAV GRAPH IS A GROUP SHARED CODE

---
+++ NavGraph.kt
@@ -0,0 +1,17 @@
+package com.cashanova.app.navigation
+
+object Routes {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val DASHBOARD = "dashboard"
+    const val BUDGET = "budget"
+    const val SET_BUDGET_GOAL = "set_budget_goal"
+    const val CREATE_CATEGORY = "create_category"
+    const val SET_CATEGORY_LIMITS = "set_category_limits"
+    const val EXPENSES = "expenses"
+    const val CREATE_EXPENSE = "create_expense"
+    const val SEARCH = "search"
+    const val EXPORT = "export"
+    const val RECURRING_EXPENSE = "recurring_expense"
+    const val INBOX = "inbox"
+}
